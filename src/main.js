bc904288673e344a9a55bf35135e5d493481d74c
Anita Kemp <anita.k.kemp@gmail.com>
2019-09-06 15:00:22 -0700

    Fix if statement for lifespan in main.js

--- src/main.js
+++ src/main.js
@@ -5,22 +5,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 $(document).ready(function(){
   $("#userAge").submit(function(event){
-    debugger;
     const userAge = parseInt($("#yourAge").val());
-    const newAge = new ageYears(userAge);
     if(Math.ceil(userAge)-userAge !==0 || userAge<0){
       alert("Invalid Input");
     } else {
+      const newAge = new ageYears(userAge);
       $("#mercuryYears").text(newAge.mercuryAge());
       $("#venusYears").text(newAge.venusAge());
       $("#marsYears").text(newAge.marsAge());
       $("#jupiterYears").text(newAge.jupiterAge());
-    }
-
-    if (userAge < 79){
-       $("#young").text(newAge.lifeExpectancy(userAge));
-    }else{
-      return $("#old").text(newAge.lifeExpectancy(userAge));
+        if (userAge < 79){
+          $("#young").text(newAge.lifeExpectancy(userAge));
+        }else{
+          return $("#old").text(newAge.lifeExpectancy(userAge));
+        }
     }
   });
 });
