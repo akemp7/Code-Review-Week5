f7831fce94d302859d16e3060afcc3601a62e3e8
Anita Kemp <anita.k.kemp@gmail.com>
2019-09-06 12:48:22 -0700

    Add more front end logic to project in main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
-export {userInput, ageYears} from './scripts.js'
+import {userInput, ageYears} from './scripts.js'
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 $(document).ready(function(){
-  
-})
+  $("#userAge").submit(function(event){
+    const userAge = $("#yourAge").val();
+
+    const newAge = new ageYears(userAge);
+    $("#mercuryYears").text(newAge.mercuryAge());
+    $("#venusYears").text(newAge.venusAge());
+    $("#marsYears").text(newAge.marsAge());
+    $("#jupiterYears").text(newAge.jupiterAge());
+  });
+});
