59fc3a55e2919f7fa42bcc5bb7e1c366a6e9b927
Anita Kemp <anita.k.kemp@gmail.com>
2019-09-06 10:22:44 -0700

    Add and pass test for determining age on Mercury

--- spec/galactic_calculator-spec.js
+++ spec/galactic_calculator-spec.js
@@ -1,19 +1,27 @@
-import {years} from './../src/scripts.js';
+import {userInput, ageYears} from './../src/scripts.js';
 describe ('userInput', function(){
   it('should refuse non-numeric input', function(){
     const input = "4%%^";
-    expect (years(input)).toEqual("Invalid Input");
+    expect (userInput(input)).toEqual("Invalid Input");
   });
   it('should only accept positive numbers', function(){
     const input = -3;
-    expect(years(input)).toEqual("Invalid Input");
+    expect(userInput(input)).toEqual("Invalid Input");
   });
   it('should only accept integers--no decimals', function(){
     const input = .5;
-    expect(years(input)).toEqual("Invalid Input");
+    expect(userInput(input)).toEqual("Invalid Input");
   });
   it('should only accept integers', function(){
     const input = 10;
-    expect(years(input)).toEqual(10);
+    expect(userInput(input)).toEqual(10);
   });
+});
+
+describe('ageYears', function(){
+  it('should divide user age by .24 and round if need be to determine age on Mercury', function(){
+    let year = new ageYears(30);
+    expect(year.mercuryAge()).toEqual(125);
+  });
+
 });
--- src/scripts.js
+++ src/scripts.js
@@ -1,4 +1,4 @@
-export function years(input){
+export function userInput(input){
   if(isNaN(input)|| input<0){
     input = "Invalid Input";
   } else if(Math.ceil(input)-input !==0){
@@ -6,4 +6,16 @@ export function years(input){
   }else {
     input = parseInt(input);
   } return input;
+}
+
+export class ageYears{
+  constructor (age, planet) {
+    this.age = age;
+    this.planet = planet;
+  }
+  mercuryAge(){
+    this.planet = .24;
+    const userAge = Math.round(this.age / this.planet);
+    return userAge;
+  }
 }
